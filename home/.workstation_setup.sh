3ffbcfacb620a489c248343f214134953d91c2e1
corey <github@krecja.net>
2018-03-27 15:16:19 -0600

    Cleaning up workstation setup, moving stuff to bashrc.d/ inux

--- home/.workstation_setup.sh
+++ home/.workstation_setup.sh
@@ -6,45 +6,41 @@ if [[ ${TRAVIS_CI_RUN} != true ]]; then
   read -n 1 -s
 fi
 
-# get my bearings
-ip_address=""
-is_debian="false"
-is_rhel="false"
-at_work="false"
-brew_options=" --default-names --with-default-names --with-gettext --override-system-vi \
-  --override-system-vim --custom-system-icons"
+export IS_OSX="false"
+export IS_LINUX="false"
+case "$(uname)" in
+  Darwin)
+    IS_OSX="true" ;;
+  Linux)
+    IS_LINUX="true" ;;
+  *)
+    echo "Unable to determine Linux or OSX" ;;
+esac
 
-if ${IS_OSX} ; then
-  ip_address=$(/sbin/ifconfig en0 | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}')
-elif ${IS_LINUX} ; then
-  # only proceed for Linux workstations
-  if [[ $(runlevel | cut -d ' ' -f2) -le 3 ]] ; then
-    echo "Quitting workstation setup on what appears to be a server"
+if ${IS_LINUX} ; then
+  # only proceed for Linux workstations, not servers
+  if [[ ! $(systemctl get-default) =~ graphical.target ]] ; then
+    echo "Quitting workstation setup on what appears to be a linux server"
     exit 0
   fi
-  ip_address=$(ip addr show | grep 'inet ' | grep -v 127 | head -n1 | awk '{print $2}' | cut -d/ -f1)
-  if [[ -f /etc/redhat-release ]] ; then
-    is_rhel="true"
-  elif [[ -f /etc/os-version ]] ; then
-    is_debian="true"
-  fi
-
-else
-  echo "Unable to determine unix distro."
 fi
 
-( [[ ${ip_address} =~ "${CUSTOM_WORK_SUBNET}.49." ]] || [[ ${ip_address} =~ "${CUSTOM_WORK_SUBNET}.200." ]] \
-  || [[ ${ip_address} =~ "${CUSTOM_WORK_SUBNET}.67." ]] ) && ${at_work}
-
-# setup passwdless sudo
-echo "Need sudo password to setup passwdless sudo"
+echo "Check for passwdless sudo"
 if ! sudo grep -q $(whoami) /etc/sudoers ; then
+  echo "Need sudo password to setup passwdless sudo"
   sudo bash -c "echo \"$(whoami) ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
 fi
 
+# install software on osx
 if ${IS_OSX} && ! hash mas 2>/dev/null ; then
   echo "Configuring OSX. This will take an hour or so."
   # see http://meng6.net/pages/computing/installing_and_configuring/installing_and_configuring_command-line_utilities/
+  if ! hash brew 2>/dev/null ; then
+    echo "Installing brew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+  brew_options=" --default-names --with-default-names --with-gettext --override-system-vi \
+    --override-system-vim --custom-system-icons"
   echo "install gnu core utilities"
   brew install coreutils
   echo "install utilities"
@@ -206,11 +202,10 @@ if [[ ${TRAVIS_CI_RUN} != true ]]; then
   #   "workbench.startupEditor": "none"
   # }
   #
-
-
-  # install cisco vpn client
-  echo "Please install the \"Cisco AnyConnect Secure Mobility Client\""
-  cat << EOF | sudo tee -a /opt/cisco/anyconnect/profile/Profile.xml
+  if [[ ${TRAVIS_CI_RUN} != true ]]; then
+    # install cisco vpn client
+    echo "Please install the \"Cisco AnyConnect Secure Mobility Client\""
+    cat << EOF | sudo tee -a /opt/cisco/anyconnect/profile/Profile.xml
 <AnyConnectProfile xmlns="http://schemas.xmlsoap.org/encoding/">
   <ServerList>
     <HostEntry>
@@ -228,7 +223,7 @@ if [[ ${TRAVIS_CI_RUN} != true ]]; then
   </ServerList>
 </AnyConnectProfile>
 EOF
-
+  fi
   # gce and gke stuff (https://cloud.google.com/sdk/docs/quickstart-mac-os-x)
   # brew install go
   # go get golang.org/x/tools/cmd/godoc
@@ -247,20 +242,14 @@ EOF
   helm init
   # minikube client (all-in-one k8s)
   # TODO: create function for installing minikube, kubectl etc (in linux) with curl+chmod+mv commands
-  minikube_version=$(curl -s https://api.github.com/repos/kubernetes/minikube/releases/latest | jq -r '.tag_name')
-  curl -Lo minikube https://storage.googleapis.com/minikube/releases/${minikube_version}/minikube-darwin-amd64
-    chmod -v +x minikube
-    sudo mv -v minikube /usr/local/bin/
-
-  # docker for mac
-  # wget https://download.docker.com/mac/stable/Docker.dmg
-  # sudo hdiutil attach Docker.img
-  # sudo installer -package /Volumes/Docker/Docker.pkg -target /Applications
-  # sudo hdiutil detach "/Volumes/Docker/"
+  # minikube_version=$(curl -s https://api.github.com/repos/kubernetes/minikube/releases/latest | jq -r '.tag_name')
+  # curl -Lo minikube https://storage.googleapis.com/minikube/releases/${minikube_version}/minikube-darwin-amd64
+  #   chmod -v +x minikube
+  #   sudo mv -v minikube /usr/local/bin/
 
   # prep for home nfs mount
-  mkdir ~/Documents/share1
-  mkdir ~/Pictures/share1
+  [[ -d ~/Documents/share1 ]] || mkdir ~/Documents/share1
+  [ -d ~/Pictures/share1 ]] || mkdir ~/Pictures/share1
 
   ### general osx customizations from https://github.com/mathiasbynens/dotfiles/blob/master/.macos ###
   # first, backup the current defaults
@@ -270,44 +259,18 @@ EOF
 
   # cloud provider credentials
   [[ -f ~/.aws_auth ]] || (umask 177 ; touch ~/.aws_auth)
-  # TODO: automatically create ~/.aws_auth by pulling secrets from lpass cli
   [[ -f ~/.gce_auth ]] || (umask 177 ; touch ~/.gce_auth)
-  # TODO: automatically create ~/.gce_auth by pulling secrets from lpass cli
 
   [[ -d ~/build ]] || mkdir ~/build
-
+  # TODO: run python code to checkout all repositories
 fi
 
-# setup git
-if ! hash git 2>/dev/null ; then
-  if ${IS_OSX} ; then
-    if ! hash brew 2>/dev/null ; then
-      echo "Installing brew"
-      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-    git config --global user.name "${CUSTOM_FULL_NAME}"
-    git config --global user.email "${CUSTOM_WORK_EMAIL}"
-  elif ${is_debian} ; then
-    sudo apt-get udpate && sudo apt-get install git -y
-    git config --global user.name "${CUSTOM_FULL_NAME}"
-    git config --global user.email "${CUSTOM_WORK_EMAIL}"
-  elif ${is_rhel} ; then
-    sudo yum install git -y
-    git config --global user.name "${CUSTOM_WORK_EMAIL/\.*/}"
-    git config --global user.email "github@${CUSTOM_HOME_DOMAIN}.net"
-  fi
-  # TODO auth github
-  # git clone https://github.com/username/repo.git
-  # Username: kr3cj
-  # Password: $(grab github personal access token from lpass-cli)
-fi
-
-# install software
-if ${IS_LINUX} && ! hash pip 2>/dev/null ; then
+# install software on linux
+if ${IS_LINUX} && ! hash packer 2>/dev/null ; then
   if ${is_debian} ; then
     echo "Configuring Debian. This will take a few minutes."
     sudo apt-get update
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    sudo apt-get install -y apt-transport-https ca-certificates curl git software-properties-common
     sudo apt-get install -y python-dev python3 tmux ack-grep jq
     # pip
     sudo wget https://bootstrap.pypa.io/get-pip.py
@@ -317,8 +280,6 @@ if ${IS_LINUX} && ! hash pip 2>/dev/null ; then
     echo "For LastPass CLI, see https://github.com/lastpass/lastpass-cli/blob/master/README.md#debianubuntu"
 
     # if you must install docker on a full blown OS
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
     sudo apt-get update
     sudo apt-get install -y docker-ce
     sudo systemctl enable docker
@@ -363,32 +324,21 @@ if ${IS_LINUX} && ! hash pip 2>/dev/null ; then
 
   elif ${is_rhel} ; then
     echo "Configuring RHEL. This will take a few minutes."
-    sudo yum install -y python-dev python3 tmux ack lastpass-cli
+    sudo yum install -y python-dev python3 tmux ack lastpass-cli git
     # pip
     sudo wget https://bootstrap.pypa.io/get-pip.py
     sudo python get-pip.py && rm get-pip.py
     sudo pip install --upgrade setuptools
   fi
   # install liquidprompt for linux
-  cd ; git clone https://github.com/nojhan/liquidprompt.git
+  cd
+  git clone https://github.com/nojhan/liquidprompt.git
   source liquidprompt/liquidprompt
   # install the rest via pip
     # sudo easy_install pip
   # vagrant, packer, python2+3, ansible, ruby, virtualbox?, gems, docker-ce, fleetctl, java, dos2unix
   # aws and gce/gke CLIs will be installed via ~/.bashrc.d/*
   sudo pip install PyYAML jinja2 paramiko ansible packer vagrant markupsafe
-fi
-
-# setup homeshick (after git); duplicated from .bootstrap.sh
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-if ! [[ -d $HOME/.homesick/repos/homeshick ]] ; then
-  git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-  source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-  homeshick clone kr3cj/dotfiles
-else
-  source $HOME/.homesick/repos/homeshick/homeshick.sh
-  # homeshick pull
-  homeshick --quiet refresh
 fi
 
 # upgrade software Fridays at 10am
