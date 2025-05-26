f0c71e886e211b91371cc979de823c56360d18d4
Corey T. <kr3cj@users.noreply.github.com>
2025-05-26 08:42:07 -0600

    More advanced socks proxy
    Ability to override ssh port
    Dynamic target address based on location

--- home/.create_socks_proxy.sh
+++ home/.create_socks_proxy.sh
@@ -37,25 +37,35 @@ fi
 
 function _socks_proxy_is_alive {
   local host1=${1}
-  local port1=${2}
-  local sourcesite="https://api.ipify.org?format=yaml" # https://checkip.dyndns.org https://ifconfig.me
-  local host1_ip="$(/usr/bin/dig @1.1.1.1 +short ${host1} | $(brew --prefix grep)/libexec/gnubin/grep '^[.0-9]*$' | tail -n1)"
-  if [[ "${host1_ip}" == "" ]]; then
-    echo "$(date -Iseconds) ERROR: unable to resolve ${host1} to an ip address; quitting"
+  local proxy_port1=${2}
+  $(brew --prefix curl)/bin/curl --connect-timeout 5 --silent \
+   --head --fail --socks5-hostname localhost:${proxy_port1} https://www.cloudflare.com/cdn-cgi/trace
+  if [[ ${?} -gt 0 ]] ; then
     return 1
-  fi
-  local response
-  response="$($(brew --prefix curl)/bin/curl --connect-timeout 5 --silent --socks5 localhost:${port1} ${sourcesite} \
-   | $(brew --prefix grep)/libexec/gnubin/grep '^[.0-9]*$' | tail -n1)"
-  if [[ "${response}" == "${host1_ip}" ]] ; then
-    return 0
   else
-    return 1
+    return 0
   fi
+
+  # local sourcesite="https://api.ipify.org?format=yaml" # https://checkip.dyndns.org https://ifconfig.me
+  # local host1_ip="$(/usr/bin/dig @1.1.1.1 +short ${host1} | $(brew --prefix grep)/libexec/gnubin/grep '^[.0-9]*$' | tail -n1)"
+  # if [[ "${host1_ip}" == "" ]]; then
+  #   echo "$(date -Iseconds) ERROR: unable to resolve ${host1} to an ip address; quitting"
+  #   return 1
+  # fi
+  # local response
+  # response="$($(brew --prefix curl)/bin/curl --connect-timeout 5 --silent --socks5 \
+  #  localhost:${proxy_port1} ${sourcesite} \
+  #  | $(brew --prefix grep)/libexec/gnubin/grep '^[.0-9]*$' | tail -n1)"
+  # if [[ "${response}" == "${host1_ip}" ]] ; then
+  #   return 0
+  # else
+  #   return 1
+  # fi
 }
 function _create_socks_proxy {
   local host1=${1}
-  local port1=${2}
+  local proxy_port1=${2}
+  local ssh_port1=${3:-"22"}
   local ssh_proxy_options="-o ServerAliveInterval=10 -o ServerAliveCountMax=20 -f -N -D "
   # local ssh_proxy_options="-f -N -D "
   [[ ${VERBOSE} -ge 1 ]] && ssh_proxy_options="-vvv ${ssh_proxy_options}"
@@ -63,18 +73,18 @@ function _create_socks_proxy {
   # clear contents of response file
   :> ${ssh_proxy_response_file}
 
-  if ! _socks_proxy_is_alive ${host1} ${port1}; then
+  if ! _socks_proxy_is_alive ${host1} ${proxy_port1}; then
     echo "$(date -Iseconds) WARN: socks proxy not connected/working; first kill any old sessions"
-    /usr/bin/pgrep -f "/usr/bin/ssh ${ssh_proxy_options}${port1}.*" && \
-    /usr/bin/pkill -f "/usr/bin/ssh ${ssh_proxy_options}${port1}.*"
+    /usr/bin/pgrep -f "/usr/bin/ssh ${ssh_proxy_options}${proxy_port1}.*" && \
+    /usr/bin/pkill -f "/usr/bin/ssh ${ssh_proxy_options}${proxy_port1}.*"
 
-    if ! port ${host1}:22 &> /dev/null; then
-      echo "$(date -Iseconds) ERROR: Skipping ${funcstack[1]}; unable to connect to \"${host1}:22\" at $(date)."
+    if ! port ${host1}:${ssh_port1} &> /dev/null; then
+      echo "$(date -Iseconds) ERROR: Skipping ${funcstack[1]}; unable to connect to \"${host1}:${ssh_port1}\" at $(date)."
       return 1
     else
-      # echo "$(date -Iseconds) INFO: attempting ssh tunnel with ${host1}:${port1} \"${host1}\")"
-      eval "/usr/bin/ssh ${ssh_proxy_options} ${port1} ${host1}"
-      [[ ${?} -eq 0 ]] && echo "$(date -Iseconds) INFO: established ssh tunnel with ${host1}:${port1}"
+      # echo "$(date -Iseconds) INFO: attempting ssh tunnel with ${host1}:${proxy_port1} \"${host1}\")"
+      eval "/usr/bin/ssh ${ssh_proxy_options} ${proxy_port1} -p ${ssh_port1} ${host1}"
+      [[ ${?} -eq 0 ]] && echo "$(date -Iseconds) INFO: established ssh tunnel with ${host1}:${proxy_port1}"
     fi
   else
     [[ ${VERBOSE} -ge 1 ]] && echo "$(date -Iseconds) INFO: socks proxy already connected/working; skipping"
@@ -96,6 +106,7 @@ else
 fi
 
 SOCKS_HOST="home.${CUSTOM_HOME_DOMAIN}"
-[[ ${at_home} == "true" ]] && SOCKS_HOST="${CUSTOM_HOME_SOCKS_LOCAL}"
+SSH_PORT="222"
+[[ ${at_home} == "true" ]] && SOCKS_HOST="${CUSTOM_HOME_SOCKS_LOCAL}" && SSH_PORT="22"
 [[ ${VERBOSE} -ge 1 ]] && echo "About to run: _socks_proxy_is_alive ${SOCKS_HOST} 2000"
-_create_socks_proxy "${SOCKS_HOST}" "2000"
+_create_socks_proxy "${SOCKS_HOST}" "2000" "${SSH_PORT}"
